046a44897751b69c72d59f1d6d0fe2480600e13f
Chanmoey <relijifa@gmail.com>
2022-07-23 22:07:43 +0800

    搭建Eureka服务。

---
+++ coupon-eureka/src/main/java/com/moon/coupon/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.moon.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Chanmoey
+ * @date 2022年07月23日
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
