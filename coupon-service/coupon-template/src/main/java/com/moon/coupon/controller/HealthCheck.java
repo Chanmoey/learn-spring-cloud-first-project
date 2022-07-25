b42f631a267ecb09de7c67d87fe6f83feac35b0c
Chanmoey <relijifa@gmail.com>
2022-07-25 20:45:00 +0800

    健康检查接口。

---
+++ coupon-service/coupon-template/src/main/java/com/moon/coupon/controller/HealthCheck.java
@@ -0,0 +1,88 @@
+package com.moon.coupon.controller;
+
+import com.moon.coupon.exception.CouponException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 健康检查接口
+ *
+ * @author Chanmoey
+ * @date 2022年07月25日
+ */
+@Slf4j
+@RestController
+public class HealthCheck {
+
+    /**
+     * 服务发现客户端
+     */
+    private final DiscoveryClient client;
+
+    /**
+     * 服务注册接口，提供了获取服务 id 的方法
+     */
+    private final Registration registration;
+
+    @Autowired
+    public HealthCheck(DiscoveryClient client, @Qualifier("eurekaRegistration") Registration registration) {
+        this.client = client;
+        this.registration = registration;
+    }
+
+    /**
+     * 健康检查
+     * localhost:7001/coupon-template/health
+     */
+    @GetMapping("/health")
+    public String health() {
+        log.debug("view health api");
+        return "CouponTemplate Is OK!";
+    }
+
+    /**
+     * 异常测试接口
+     * localhost:7001/coupon-template/exception
+     */
+    @GetMapping("/exception")
+    public String exception() throws CouponException {
+        log.debug("view exception api");
+        throw new CouponException("CouponTemplate Has Some Problem");
+    }
+
+    /**
+     * 获取 Eureka Server 上的微服务信息
+     * localhost:7001/coupon-template/info
+     */
+    @GetMapping("/info")
+    public List<Map<String, Object>> info() {
+
+        // 大学需要等待2分钟才能够获取到注册信息
+
+        // 返回本服务的所有实例，因为一个服务可以部署多个实例。
+        List<ServiceInstance> instances = client.getInstances(registration.getServiceId());
+        List<Map<String, Object>> result = new ArrayList<>(instances.size());
+
+        instances.forEach(i -> {
+            Map<String, Object> info = new HashMap<>();
+            info.put("serviced", i.getServiceId());
+            info.put("instanceId", i.getInstanceId());
+            info.put("port", i.getPort());
+
+            result.add(info);
+        });
+
+        return result;
+    }
+}
